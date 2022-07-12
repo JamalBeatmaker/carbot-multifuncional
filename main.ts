45dc11a2e19e82e57e68e55d74c32445da8f2e99
JamalBeatmaker <89939812+JamalBeatmaker@users.noreply.github.com>
2022-07-11 22:25:46 -0300

    codigo prof wagner

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
 led.enable(false)
 basic.forever(function () {
-    if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, false)) {
+    if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, true)) {
         hackbitmotors.MotorRunDual(
         hackbitmotors.Motors.M1A,
-        50,
+        70,
         hackbitmotors.Motors.M1B,
-        50
+        70
+        )
+    } else if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, false)) {
+        hackbitmotors.MotorStopAll()
+    } else if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, true)) {
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        0,
+        hackbitmotors.Motors.M1B,
+        40
         )
     } else if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, false)) {
-        hackbitmotors.MotorRun(hackbitmotors.Motors.M1A, 50)
-        hackbitmotors.MotorStop(hackbitmotors.Motors.M1B)
-    } else if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, true)) {
-        hackbitmotors.MotorRun(hackbitmotors.Motors.M1B, 50)
-        hackbitmotors.MotorStop(hackbitmotors.Motors.M1A)
-    } else if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, true)) {
-        hackbitmotors.MotorStopAll()
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        40,
+        hackbitmotors.Motors.M1B,
+        0
+        )
     }
 })
