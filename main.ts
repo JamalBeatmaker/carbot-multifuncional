411a6932c6147c603affea233c513aabca72e6aa
Jamal Beatmaker <89939812+JamalBeatmaker@users.noreply.github.com>
2023-06-15 12:55:55 -0300

    Função segue e linha e parar no obstaculo

--- main.ts
+++ main.ts
@@ -1,27 +1,48 @@
-led.enable(false)
-basic.forever(function () {
-    if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, true)) {
-        hackbitmotors.MotorRunDual(
-        hackbitmotors.Motors.M1A,
-        70,
-        hackbitmotors.Motors.M1B,
-        70
-        )
-    } else if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, false)) {
-        hackbitmotors.MotorStopAll()
-    } else if (hackbitsensor.detectline(DigitalPin.P4, false) && hackbitsensor.detectline(DigitalPin.P10, true)) {
-        hackbitmotors.MotorRunDual(
-        hackbitmotors.Motors.M1A,
-        0,
-        hackbitmotors.Motors.M1B,
-        40
-        )
-    } else if (hackbitsensor.detectline(DigitalPin.P4, true) && hackbitsensor.detectline(DigitalPin.P10, false)) {
+function segue_linha () {
+    if (hackbit.detectline(DigitalPin.P4, true) && hackbit.detectline(DigitalPin.P10, true)) {
         hackbitmotors.MotorRunDual(
         hackbitmotors.Motors.M1A,
         40,
         hackbitmotors.Motors.M1B,
-        0
+        40
         )
+    } else if (hackbit.detectline(DigitalPin.P4, false) && hackbit.detectline(DigitalPin.P10, false)) {
+        basic.pause(100)
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        50,
+        hackbitmotors.Motors.M1B,
+        50
+        )
+    } else if (hackbit.detectline(DigitalPin.P4, false) && hackbit.detectline(DigitalPin.P10, true)) {
+        basic.pause(100)
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        -95,
+        hackbitmotors.Motors.M1B,
+        45
+        )
+    } else if (hackbit.detectline(DigitalPin.P4, true) && hackbit.detectline(DigitalPin.P10, false)) {
+        basic.pause(100)
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        45,
+        hackbitmotors.Motors.M1B,
+        -90
+        )
+    }
+}
+let distancia2 = 0
+led.enable(false)
+basic.forever(function () {
+    distancia2 = hackbit.us_sonar(
+    DigitalPin.P13,
+    DigitalPin.P14,
+    hackbit.PingUnit.cm
+    )
+    serial.writeValue("Distancia", distancia2)
+    segue_linha()
+    if (distancia2 <= 10) {
+        hackbitmotors.MotorStopAll()
     }
 })
